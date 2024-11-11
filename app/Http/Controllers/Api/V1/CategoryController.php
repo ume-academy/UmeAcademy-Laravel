addf54443a4270b3cb8f3b44473ac6eb99fe53c7
minhnguyet27 <mihnguyt2@gmail.com>
2024-11-11 10:50:25 +0700

    #UME-144: API lấy danh sách khóa học

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Http\Controllers\Api\V1;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\CategoryResource;
+use App\Services\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller 
+{
+    public function __construct(
+        private CategoryService $categoryService,
+    ){}
+    public function getAllCategories(Request $req){
+        try {
+            $perPage = $req->input('per_page', 10);
+            $categories = $this->categoryService->getAllCategories($perPage);
+            return new  CategoryResource($categories);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
+    }
+}
---
+++ app/Repositories/Interfaces/CategoryRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface CategoryRepositoryInterface
+{
+    // 
+    public function all($perPage);
+}
