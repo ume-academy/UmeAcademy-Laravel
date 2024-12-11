18d8ce5b4d097196498b1c320b32f63a49975f25
VanHai1424 <132867558+VanHai1424@users.noreply.github.com>
2024-12-11 11:05:54 +0700

    #UME-344 feat: Thêm thời gian hết hạn của đơn hàng

--- app/Http/Resources/Refund/RefundResource.php
+++ app/Http/Resources/Refund/RefundResource.php
@@ -18,7 +18,11 @@ class RefundResource extends JsonResource
             'id' => $this->id ?? null,
             'transaction_code' => $this->transaction_code ?? null,
             'refund_reason' => $this->refund_reason ?? null,
-            'transaction' => $this->transaction ?? null,
+            'price' => $this->transaction->discount_price ?? null,
+            'student' => $this->transaction->user->fullname ?? null,
+            'course' => $this->transaction->course->name ?? null,
+            'teacher' => $this->transaction->course?->teacher?->user?->fullname ?? null,
+            'price' => $this->transaction->discount_price ?? null,
             'status' => $this->status ?? null,
             'created_at' => $this->created_at ?? null,
         ];
